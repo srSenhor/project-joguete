0c59b7fa0d9ac872df450a214ca5dd8b89fb8a99
srSenhor <viniciusfeliciano14@gmail.com>
2023-10-16 11:42:54 -0300

    seleção de assets e configs na main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:projeto_joguete/joguete.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setLandscape();
+
   Joguete game = Joguete();
   runApp(GameWidget(game: game));
 }
