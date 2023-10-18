154597eefa27bcf69b67c32d9aaaf2cea5c5c63b
srSenhor <viniciusfeliciano14@gmail.com>
2023-10-17 21:28:53 -0300

    ajustes na posição

--- lib/character.dart
+++ lib/character.dart
@@ -2,6 +2,7 @@ import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/sprite.dart';
+import 'package:projeto_joguete/props/spike_trap.dart';
 import 'joguete.dart';
 
 class Character extends SpriteAnimationComponent
@@ -15,14 +16,38 @@ class Character extends SpriteAnimationComponent
   double ax = 0;
   double ay = 0;
   bool gameOver = false;
+  /*
+  late SpriteSheet runSpriteSheet, jumpSpriteSheet, fallSpriteSheet;
+  late SpriteAnimation runAnimation, jumpAnimation, fallAnimation;
+  */
   late SpriteSheet idleSpriteSheet, hitSpriteSheet;
   late SpriteAnimation idleAnimation, hitAnimation;
-
   @override
   void onLoad() async {
-    position = Vector2(48, 340);
-    size = Vector2(64.0, 64.0);
+    position = Vector2(48, gameRef.size.y - size.y - 77);
+    size = Vector2.all(64.0);
     anchor = Anchor.center;
+    /*
+    runSpriteSheet = SpriteSheet(
+      image: await gameRef.images.load('Run.png'),
+      srcSize: Vector2.all(32.0),
+    );
+    jumpSpriteSheet = SpriteSheet(
+      image: await gameRef.images.load('Jump.png'),
+      srcSize: Vector2.all(32.0),
+    );
+    fallSpriteSheet = SpriteSheet(
+      image: await gameRef.images.load('Fall.png'),
+      srcSize: Vector2.all(32.0),
+    );
+    runAnimation = runSpriteSheet.createAnimation(
+        row: 0, stepTime: 0.2, from: 0, to: 6, loop: true);
+    jumpAnimation = jumpSpriteSheet.createAnimation(
+        row: 0, stepTime: 0.2, from: 0, to: 10, loop: true);
+    fallAnimation = fallSpriteSheet.createAnimation(
+        row: 0, stepTime: 0.2, from: 0, to: 10, loop: true);
+    animation = runAnimation;
+    */
     idleSpriteSheet = SpriteSheet(
       image: await gameRef.images.load('Idle.png'),
       srcSize: Vector2.all(32.0),
@@ -43,6 +68,7 @@ class Character extends SpriteAnimationComponent
   void onTapUp(TapUpEvent event) async {
     jump();
     animation = hitAnimation;
+    //animation = jumpAnimation;
   }
 
   @override
@@ -55,6 +81,19 @@ class Character extends SpriteAnimationComponent
     vy = -200;
   }
 
+  /*
+  @override
+  void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
+    super.onCollision(intersectionPoints, other);
+
+    if (other is SpikeTrap) {
+      gameOver = true;
+      ay = 0;
+      vy = 0;
+      removeFromParent();
+    }
+  }
+  */
   @override
   void update(double dt) {
     super.update(dt);
